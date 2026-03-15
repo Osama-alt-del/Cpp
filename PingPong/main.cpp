44779829e943c045cc4b286ed9e0f8a9b8724130
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-15 22:32:01 +0900

    So I've noticed a lot of things so far.
     - Firstly, I've noticed that we can use the C standard libraries in C++ code
     - Secondly, I've noticed that C++ takes noticably longer to compile than C (due to added complexity)
     - Regarding the notes I've made on how a game is made, I didn't know that it was the OS that talked to the drivers, and all our software does is talk to the OS. That's new information to me.

---
+++ PingPong/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h> // We can use C libraries in C++
+#include <iostream> // We don't have to use iostream.h
+
+
+int main(){
+    std::cout << "Hello World, this is literally so much more complicated that just printf, but ok lol" <<std::endl;
+    return 0;
+}
