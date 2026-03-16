44dbfe14a64a40810824cdb6fffa942758e89c52
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-16 16:18:42 +0900

    Making platform dependent code for windows

---
+++ PingPong/win32_platform.cpp
@@ -0,0 +1,19 @@
+// This is the platform dependent code for the game:
+// So first we are going to include the windows library:
+#include <windows.h>
+
+// So win32 is an API that allows devs to create applications for the Windows Operating System
+// We can search the documentation for win32, by typing in winmain msdn (microsoft developer network)
+// We are going to try and implement WinMain, which is entry point (where the program starts) for graphical windows-based applications
+// Here is the function and it's parameters:
+
+int __clrcall WinMain(
+  [in]           HINSTANCE hInstance,
+  [in, optional] HINSTANCE hPrevInstance,
+  [in]           LPSTR     lpCmdLine,
+  [in]           int       nShowCmd
+){
+
+}
+
+
